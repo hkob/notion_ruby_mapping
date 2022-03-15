542291dfe8c6732f9bfbb4696f7e26ad91c10e14
Hiroyuki KOBAYASHI <hkob@metro-cit.ac.jp>
2022-03-15 21:52:55 +0900

    Fixed not to reload from API when all contents are loaded

--- lib/notion_ruby_mapping/list.rb
+++ lib/notion_ruby_mapping/list.rb
@@ -8,6 +8,7 @@ module NotionRubyMapping
     def initialize(json: nil, id: nil, database: nil, query: nil)
       super(json: json, id: id)
       @has_more = @json["has_more"]
+      @load_all_contents = !@has_more
       @database = database
       @query = query
       @index = 0
@@ -18,30 +19,34 @@ module NotionRubyMapping
     def each
       return enum_for(:each) unless block_given?
 
-      unless @has_content # re-exec
-        @query.start_cursor = nil
-        @json = @database.query_database @query
-        @index = 0
-        @has_more = @json["has_more"]
-        @has_content = true
-      end
-
-      while @has_content
-        if @index < results.length
-          object = Base.create_from_json(results[@index])
-          @index += 1
-          yield object
-        elsif @has_more
-          if @database
-            @query.start_cursor = @json["next_cursor"]
+      if @database
+        unless @has_content # re-exec
+          unless @load_all_contents
+            @query.start_cursor = nil
             @json = @database.query_database @query
-            @index = 0
             @has_more = @json["has_more"]
+          end
+          @index = 0
+          @has_content = true
+        end
+
+        while @has_content
+          if @index < results.length
+            object = Base.create_from_json(results[@index])
+            @index += 1
+            yield object
+          elsif @has_more
+            if @database
+              @query.start_cursor = @json["next_cursor"]
+              @json = @database.query_database @query
+              @index = 0
+              @has_more = @json["has_more"]
+            else
+              @has_content = false
+            end
           else
             @has_content = false
           end
-        else
-          @has_content = false
         end
       end
     end
